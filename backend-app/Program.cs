85072a75ffdb2fc5ecd514f70b4b9fb7163fa8ac
MCMO_PC <z5309586@ad.unsw.edu.au>
2023-01-26 01:41:19 +1100

    Added property class

--- backend-app/Program.cs
+++ backend-app/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to the Carcassonne Backend!");
+﻿using System;
+
+namespace Carcassonne {
+
+    class Program {
+        static void Main(string[] args) {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Welcome to the Carcassonne Backend!");
+            Property test = new Property("Randwick", "14 Botany Street", 400, 2, 2, 2);
+            Console.WriteLine("The suburb is on {0} in {1}", test.Suburb, test.Street);
+            Console.WriteLine("The property rents for ${0} a week and has {1} bedrooms, {2} bathrooms and {3} garage spaces", 
+            test.Price, test.Num_bedroom, test.Num_bathroom, test.Num_garage);
+            Console.WriteLine(test.Street);
+        }
+    }
+}
---
+++ backend-app/Property.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Carcassonne {
+    class Property {
+        private string suburb;
+        private string street;
+        private int price;
+        private int num_bedroom;
+        private int num_bathroom;
+        private int num_garage;
+
+        public Property(string suburb, string street, int price, int num_bedroom, int num_bathroom, int num_garage) {
+            this.suburb = suburb;
+            this.street = street;
+            this.price = price;
+            this.num_bedroom = num_bedroom;
+            this.num_bathroom = num_bathroom;
+            this.num_garage = num_garage;
+        }
+
+        public string Suburb { get {return suburb;} set{ suburb = value;} }
+        public string Street { get {return street;} set{ street = value;} }
+        public int Price { get {return price;} set{ price = value;} }
+        public int Num_bedroom { get {return num_bedroom;} set{ num_bedroom = value;} }        
+        public int Num_bathroom { get {return num_bathroom;} set{ num_bathroom = value;} }
+        public int Num_garage { get {return num_garage;} set{ num_garage = value;} }
+    }
+}
